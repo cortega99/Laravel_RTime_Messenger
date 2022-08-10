f6f3a172f788a0150aaad850836cef77406d975a
Claudio Ortega <polloortega15@gmail.com>
2022-08-10 15:33:20 -0500

    Vue.js integration, updating webpack.mix.js, adding authentication

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
 require('./bootstrap');
+
+// Require Vue
+window.Vue = require('vue').default;
+
+// Register Vue Components
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+// Initialize Vue
+const app = new Vue({
+    el: '#app',
+});
